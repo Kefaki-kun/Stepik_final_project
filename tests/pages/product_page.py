c64a6e8d4738a5af935e4a66d01b6c47b98aac2d
Karina Danilchuk <karina@highstreetmobile.com>
2021-03-08 20:40:49 +0300

    Changed the test for a promo_offers on the product page

---
+++ tests/pages/product_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_THE_CART_BUTTON)
+        button.click()
+
+    def guest_can_see_product_name(self):
+        product_name_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_CART)
+        product_name_in_store = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_STORE)
+        assert product_name_in_store.text == product_name_in_cart.text, \
+            "Different names for a product in store and in cart"
+
+    def product_price_is_correct(self):
+        product_price_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_CART)
+        product_price_in_store = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_STORE)
+        assert product_price_in_store.text == product_price_in_cart.text, "Incorrect price"
+
+    def check_message_about_adding_to_cart(self):
+        cart_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE)
+        assert cart_message and self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def check_success_message_on_product_page(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def check_success_message_is_disappeared(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
---
+++ tests/test_product_page.py
@@ -0,0 +1,21 @@
+import time
+
+import pytest
+
+from .pages.product_page import ProductPage
+
+
+@pytest.mark.parametrize('promo_offer',
+                         ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4",
+                          "?promo=offer5", "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                          "?promo=offer8", "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo_offer}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.solve_quiz_and_get_code()
+    time.sleep(2)
+    page.guest_can_see_product_name()
+    time.sleep(2)
+    page.product_price_is_correct()
