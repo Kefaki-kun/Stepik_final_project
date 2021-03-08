f770f695194a099c1f5a2012204150ec43e2316d
Karina Danilchuk <karina@highstreetmobile.com>
2021-03-08 19:53:35 +0300

    Added test for add a product to cart (guest user, product page)

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -9,3 +9,12 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_THE_CART_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME_IN_CART = (By.CSS_SELECTOR, "#messages div.alertinner strong")
+    PRODUCT_NAME_IN_STORE = (By.CSS_SELECTOR, "#content_inner h1:first-child")
+    PRODUCT_PRICE_CART = (By.CSS_SELECTOR, ".alert-info p:first-child strong")
+    PRODUCT_PRICE_STORE = (By.CSS_SELECTOR, "#content_inner p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div.alertinner")
