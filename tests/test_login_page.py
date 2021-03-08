b06ac140cf34071141f635ae268c42bb1582735e
Karina Danilchuk <karina@highstreetmobile.com>
2021-03-08 16:01:36 +0300

    added methods and locators for the Login page (log in and register forms), added tests for the Login page

---
+++ tests/test_login_page.py
@@ -0,0 +1,22 @@
+from .pages.login_page import LoginPage
+
+
+def test_can_open_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_login_form_displaying(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_register_form_displaying(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
