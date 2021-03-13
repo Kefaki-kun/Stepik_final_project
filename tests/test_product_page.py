0ce95ff27bb0a3b9db07cb4536cb0839cd9e1352
Karina Danilchuk <karina@highstreetmobile.com>
2021-03-13 21:10:10 +0300

    Marks refactoring

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -7,11 +7,12 @@ from .pages.cart_page import CartPage
 from .pages.login_page import LoginPage
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer',
                          ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4",
                           "?promo=offer5", "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail),
                           "?promo=offer8", "?promo=offer9"])
+# original name: test_guest_can_add_product_to_basket
 def test_guest_can_add_product_to_cart(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
@@ -24,7 +25,6 @@ def test_guest_can_add_product_to_cart(browser, promo_offer):
     page.product_price_is_correct()
 
 
-@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -35,7 +35,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     page.check_message_about_adding_to_cart()
 
 
-@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -46,14 +45,13 @@ def test_message_disappeared_after_adding_product_to_cart(browser):
     page.check_success_message_is_disappeared()
 
 
-@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.skip
+
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -61,14 +59,15 @@ def test_guest_cant_see_success_message(browser):
     page.check_success_message_on_product_page()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.skip
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = CartPage(browser, link)
@@ -86,7 +85,7 @@ class TestUserAddToBasketFromProductPage():
         link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
         self.page = LoginPage(browser, link)
         self.page.open()
-        self.page.register_new_user (email, password, browser)
+        self.page.register_new_user(email, password, browser)
         self.page.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
@@ -95,11 +94,14 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.check_success_message_on_product_page()
 
+    @pytest.mark.need_review
+    # original name: test_user_can_add_product_to_basket
     def test_user_can_add_product_to_cart(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.add_product_to_cart()
+        page.solve_quiz_and_get_code()
         page.guest_can_see_product_name()
         time.sleep(2)
         page.product_price_is_correct()
